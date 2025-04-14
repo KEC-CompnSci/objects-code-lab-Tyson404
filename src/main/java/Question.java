d896c9fdd4e62f09f38331b1fdff67eabbb25133
Tyson404 <25quangt@retsdstudent.ca>
2025-04-14 16:05:59 +0000

    done i think

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -2,5 +2,29 @@ import java.util.Scanner;
 
 
 public class Question {
-  
+    private String question;
+    public int totalMarks;
+    private String answer;
+    private String studentAnswer;
+
+    public Question(String question, int totalMarks, String answer){
+        this.question = question;
+        this.totalMarks = totalMarks;
+        this.answer = answer;
+        this.studentAnswer = "";
+    }
+    public void AskQuestion(Scanner scanner){
+        System.out.println("Question: " + question + " (" + totalMarks + " marks)");
+        System.out.print("Your answer: ");
+        studentAnswer = scanner.nextLine();
+        System.out.println(studentAnswer);
+    }
+
+    public int CheckAnswer(){
+        if (studentAnswer.equals(answer)) {
+            return totalMarks;
+        } else {
+            return 0;
+        }
+    }
 }
